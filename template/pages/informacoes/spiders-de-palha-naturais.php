476dbeead609b6b3bc932378e41f55199fc4acc7
Agencia-4UP <116680983+Agencia-4UP@users.noreply.github.com>
2023-09-08 14:00:14 -0300

    Add files via upload

---
+++ template/pages/informacoes/spiders-de-palha-naturais.php
@@ -0,0 +1,50 @@
+<?php 
+
+$keywords	= 'Spiders de palha naturais';
+$desc		= 'Seu fornecimento de Spiders de palha naturais com excelência está na Noguti, uma empresa que dedica-se a sua satisfação.';
+$keyfrases	= '';
+$keyregiao	= 'Spiders de palha naturais';
+$file		= 'spiders-de-palha-naturais';
+
+$linkLoja	= 'https://nogutichinelos.com.br/todososprodutos';
+$linkPinterest	= 'imagem_pinterest';
+
+$title		= 'Spiders de palha naturais';
+$text_title	= '<p>Você está procurando por <strong>Spiders de palha naturais</strong>? Este tipo de calçado é fundamental e pode ser utilizado como lembranças para várias ocasiões, podendo ser oferecido como brinde tanto para homens quanto para mulheres. É adequado para ser distribuído em clínicas de estética, salões de beleza, feiras e eventos, festas de formatura, casamentos, aniversários, confraternizações e também em datas especiais, como o Natal e o Ano Novo. Além disso, pode ser associado a estratégias de publicidade e marketing. Como resultado, a procura por fornecedores <strong>Spiders de palha naturais</strong> tem aumentado.</p>
+
+<p>Além disso, os <strong>Spiders de palha naturais</strong> também são uma excelente opção de presente, pois oferecem versatilidade, havendo diferentes modelos que atendem a uma ampla gama de públicos. Desde jovens a idosos, tanto homens quanto mulheres podem desfrutar desses presentes, tornando a escolha adequada fácil e versátil.</p>';
+
+$text_title02	= '<p>Para isso, é preciso ter um fornecedor confiável no mercado, que seja capaz de oferecer:</p>
+
+<ul>
+	<li>Atendimento ágil e especializado;</li>
+	<li>Eficiência no atendimento com os produtos;</li>
+	<li>Personalização de excelência;</li>
+	<li>Preços competitivos e justos;</li>
+</ul>
+
+<p>No mercado a <a class="tm-redirecionamento" href="https://nogutichinelos.com.br">Noguti Chinelos</a> se destaca ao proporcionar aos seus clientes <strong>Spiders de palha naturais</strong> de alta qualidade, livre de látex, a preços acessíveis, acompanhado de um atendimento diferenciado. Somos especializados no ramo de chinelos, comprometidos em fornecer produtos de excelência, sempre buscando atender às necessidades dos nossos clientes.</p>';
+
+$subtitulo    = 'Spiders de palha naturais: Com os melhores preços';
+$text_subtitle	= '<p>Decidiu buscar um fornecedor de <strong>Spiders de palha naturais</strong> conosco? Então você está optando por uma empresa focada em sustentabilidade. Nossos processos utilizam a melhor tecnologia e técnicas para otimizar os resultados para você. Seja no fornecimento de <strong>Spiders de palha naturais</strong> ou qualquer outra opção você tem o melhor produto. Desde 1980, estamos no mercado para oferecer ao cliente o que ele merece. Nossa localização é Rua José Dias de Araújo, 93 Vila Ribeirópolis Registro SP, mas se ficar muito longe para você, temos a opção de compra online para maior comodidade e conforto. Slides, pantufas, chinelos calçados de cores lisas, animal print, neon, poás e entre tantos outros produtos lindos e que combinam com qualquer situação em que você calça-los. Aliando a tecnologia à tranquilidade de escolher onde quiser e com facilidade, temos o nosso Whatsapp que é o número <a class="tm-whats"href="https://api.whatsapp.com/send?phone=551338226391">13 3822-6391</a> para te ajudar na hora da compra. Tudo isto para ajudar o consumidor a escolher o que quiser por um preço justo. Venha conferir as novidades em nossas redes sociais, no Instagram, estamos no Instagram @nogutichinelos e no Facebook como Noguti.</p>
+
+<p>Em nosso site, você pode encontrar ofertas e opções para diferentes gostos e públicos, encontre o modelo ideal para a ocasião certa. Estamos sempre antenados na moda e optando por produtos com alta qualidade e com padrão de moda para todas as faixas-etárias da família e com preços que cabem em todos os bolsos. Não perca tempo e venha nos conhecer, mesmo que seja online. Estamos te esperando e queremos fazer parte da sua rotina mais confortável. Venha fazer o seu <strong>Spiders de palha naturais</strong> conosco.</p>';
+
+$json =  json_encode(
+	array(
+		"file_name"			=> basename($_SERVER["REQUEST_URI"]),
+		"keywords"			=> $keywords,
+		"description"		=> $desc,
+		"keyfrases"			=> $keyfrases,
+		"keyregiao"			=> $keyregiao,
+		"title"				=> $title,
+		"text_title"		=> $text_title,
+		"text_title02"		=> $text_title02,
+		"subtitle"			=> $subtitulo,
+		"text_subtitle"		=> $text_subtitle,
+		"linkLoja"			=> $linkLoja,
+		"linkPinterest"		=> $linkPinterest,
+	));
+
+	echo $json;
+?>
